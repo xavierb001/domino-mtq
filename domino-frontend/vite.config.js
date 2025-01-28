bde3f32d620ae9e193055503de838df727f6f8b7
Xavier BEAUNOL <xavier.beaunol001@gmail.com>
2025-01-28 23:21:12 +0100

    Fix dependencies and update vite.config.js

--- domino-frontend/vite.config.js
+++ domino-frontend/vite.config.js
@@ -1,6 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3000',
+      '/socket.io': {
+        target: 'http://localhost:3000',
+        ws: true
+      }
+    }
+  }
+});
 // Configuration du proxy pour rediriger les requêtes API vers le backend
 /*export default defineConfig({
   plugins: [react()],
@@ -18,15 +29,3 @@ import react from '@vitejs/plugin-react';
   },
 });*/
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:3000',
-      '/socket.io': {
-        target: 'http://localhost:3000',
-        ws: true
-      }
-    }
-  }
-});
