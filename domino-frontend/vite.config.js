1044026c2b202e0f0e7f56ecb3954f869d4456b2
Xavier BEAUNOL <xavier.beaunol001@gmail.com>
2025-01-29 11:55:02 +0100

    Fix CORS configuration and update vite.config.js

--- domino-frontend/vite.config.js
+++ domino-frontend/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': 'http://localhost:3000',
-      changeOrigin: true,
+      //changeOrigin: true,
       //secure: false,
       '/socket.io': {
         target: 'http://localhost:3000',
