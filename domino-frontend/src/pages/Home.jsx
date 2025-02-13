04ab55d32bb1575e8a0b7b489faa3a2cff06052d
Xavier BEAUNOL <xavier.beaunol001@gmail.com>
2025-02-13 16:17:49 +0100

    add rules

--- domino-frontend/src/pages/Home.jsx
+++ domino-frontend/src/pages/Home.jsx
@@ -72,74 +72,13 @@ function Home() {
   <button onClick={joinGame} style={{ padding: '10px 20px' }}>
     Rejoindre
   </button>
-
-
- 
-      {/* Bouton d'information */}
-      <img
-        src="https://res.cloudinary.com/dwvfz8o89/image/upload/f_auto,q_auto/v1/domino/cpengjxws12jdjvxsk5m"
-        alt="Règles du jeu"
-        onClick={toggleRulesPopup}
-        style={{
-          position: "absolute",
-          top: "20px",
-          right: "20px",
-          width: "40px",
-          height: "40px",
-          cursor: "pointer",
-        }}
-      />
-
-      {/* Popup des règles */}
-      {showRules && (
-        <div
-          style={{
-            position: "fixed",
-            top: "50%",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
-            backgroundColor: "white",
-            padding: "20px",
-            borderRadius: "10px",
-            boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
-            width: "80%",
-            maxWidth: "400px",
-            zIndex: 1000,
-          }}
-        >
-          <h2>📜 Règles du Jeu</h2>
-          <p>
-            🎲 **Nombre de joueurs** : 3 joueurs<br />
-            🃏 **Distribution** : Chaque joueur reçoit 7 dominos.<br />
-            ➡️ **Déroulement** : Les joueurs posent un domino qui correspond à l’une des extrémités du plateau.<br />
-            🚫 **Tour passé** : Si un joueur ne peut pas jouer, il passe son tour.<br />
-            🏆 **Victoire** : Le premier joueur à ne plus avoir de dominos gagne la manche !<br />
-            ❌ **Partie bloquée** : Si personne ne peut jouer, la manche est annulée.<br />
-          </p>
-
-          <button
-            onClick={toggleRulesPopup}
-            style={{
-              marginTop: "10px",
-              padding: "10px 20px",
-              backgroundColor: "#007bff",
-              color: "white",
-              border: "none",
-              borderRadius: "5px",
-              cursor: "pointer",
-            }}
-          >
-            Fermer
-          </button>
-        </div>
-      )}
-    </div>
+  
   
   </div>
     </div>
     </div>
     </div>
-  
+    </div>
   );
 }
 
